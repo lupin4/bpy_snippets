12ed85ab2ad81d14b95ebbe32fec0fd7539302d3
Satish Goda <satishgoda@live.com>
2022-03-09 22:32:56 -0800

    Create active_operator.py
    bpy.context.active_operator

---
+++ context/active_operator.py
@@ -0,0 +1,20 @@
+>>> C.active_operator
+<bpy_struct, Operator("MESH_OT_primitive_cube_add") at 0x00000202AAC11E48>
+
+>>> C.active_operator.bl_cursor_pending
+'PICK_AREA'
+
+>>> C.active_operator.bl_description
+'Construct a cube mesh'
+
+>>> C.active_operator.bl_idname
+'MESH_OT_primitive_cube_add'
+
+>>> C.active_operator.bl_label
+'Add Cube'
+
+>>> C.active_operator.bl_options
+{'UNDO', 'REGISTER'}
+
+>>> C.active_operator.bl_undo_group
+''
